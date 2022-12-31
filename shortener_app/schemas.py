c4ab1fbe32d0c07bdb0f34074a789902c2148af6
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-12-31 12:13:13 +0000

    Fourteenth Commit: Implement schemas for request and response body

---
+++ shortener_app/schemas.py
@@ -0,0 +1,21 @@
+# schemas.py
+# Schema for request body and response body
+
+from pydantic import BaseModel
+
+
+class URLBase(BaseModel):
+    target_url = str
+
+
+class URL(URLBase):
+    is_active: bool
+    clicks: int
+
+    class Config:
+        orm_mode = True
+
+
+class URLInfo(URL):
+    url: str
+    admin_url: str
