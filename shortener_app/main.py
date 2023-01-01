4cab9364411b2ec78c03dc1a4da6c4df8b5d0b62
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-01-01 10:28:25 +0000

    Twenty-One Commit: Refactor function that handles post request for creating shortened URL

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -31,8 +31,21 @@ def read_root():
     return "Welcome to the URL shortener API ;-)"
 
 
-@app.post("/url")
-def create_url(url: schemas.URLBase):
+@app.post("/url", response_model=schemas.URLInfo)
+def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
     if not validators.url(url.target_url):
         raise_bad_request(message="Your provided URL is not valid")
-    return f"TODO: Create database entry for: {url.target_url}"
+
+    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    key = "".join(secrets.choice(chars) for _ in range(5))
+    secret_key = "".join(secrets.choice(chars) for _ in range(8))
+    db_url = models.URL(
+        target_url=url.target_url, key=key, secret_key=secret_key
+    )
+    db.add(db_url)
+    db.commit()
+    db.refresh(db_url)
+    db_url.url = key
+    db_url.admin_url = secret_key
+
+    return db_url
