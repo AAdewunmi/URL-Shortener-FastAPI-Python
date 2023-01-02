ee8391fef791b0920b2ff415d73c8e9321cf0d72
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-01-02 14:50:45 +0000

    Thirty-One Commit: Refactor create_db_url() to make sure every shortened URL exists only once.

--- shortener_app/crud.py
+++ shortener_app/crud.py
@@ -6,8 +6,8 @@ from . import keygen, models, schemas
 
 
 def create_db_url(db: Session, url: schemas.URLBase) -> models.URL:
-    key = keygen.create_random_key()
-    secret_key = keygen.create_random_key(length=8)
+    key = keygen.create_unique_random_key(db)
+    secret_key = f"{key}_{keygen.create_random_key(length=8)}"
     db_url = models.URL(
         target_url=url.target_url, key=key, secret_key=secret_key
     )
