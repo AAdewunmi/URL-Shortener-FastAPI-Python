376cdd3d591e4eea6ddd978ce633f74ac2da5796
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-01-02 13:08:11 +0000

    Twenty-Six Commit: Implement helper function to generate keys for URLs

--- shortener_app/keygen.py
+++ shortener_app/keygen.py
@@ -1,2 +1,10 @@
 # keygen.py
-#
+# Helper function to generate keys for URLs
+
+import secrets
+import string
+
+
+def create_random_key(length: int = 5) -> str:
+    chars = string.ascii_uppercase + string.digits
+    return "".join(secrets.choice(chars) for _ in range(length))
