05544c462358eacfd6efe52479ce8e2dc03956c0
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-01-03 07:49:38 +0000

    Thirty-Six Commit: Implement get_url_info() function

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -64,4 +64,12 @@ def forward_to_target_url(
     name="administration info",
     response_model=schemas.URLInfo,
 )
-
+def get_url_info(
+        secret_key: str, request: Request, db: Session = Depends(get_db)
+):
+    if db_url := crud.get_db_url_by_secret_key(db, secret_key=secret_key):
+        db_url.url = db_url.key
+        db_url.admin_url = db_url.secret_key
+        return db_url
+    else:
+        raise_not_found(request)
