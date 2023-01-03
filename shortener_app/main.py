c95bc5cdd4d9601a577b61eaf4aea8b995f27d85
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-01-03 07:48:18 +0000

    Thirty-Five Commit: Implement a new API endpoint at the /admin/{secret_key} URL

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -52,8 +52,16 @@ def forward_to_target_url(
         url_key: str,
         request: Request,
         db: Session = Depends(get_db)
-    ):
+):
     if db_url := crud.get_db_url_by_key(db=db, url_key=url_key):
         return RedirectResponse(db_url.target_url)
     else:
         raise_not_found(request)
+
+
+@app.get(
+    "/admin/{secret_key}",
+    name="administration info",
+    response_model=schemas.URLInfo,
+)
+
